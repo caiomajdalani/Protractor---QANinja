43086a49d023342e1b4b8c59284ef42729e1cf28
Caio Majdalani <caio.majdalani@gmail.com>
2017-08-01 11:37:03 -0300

    Customer and Invoices

--- pages/customer_po.js
+++ pages/customer_po.js
@@ -4,6 +4,16 @@ class Customer {
     constructor(){
         this.newCustomer = $('#dataview-insert-button');
         this.alertInfo = $('#content div[class*=alert-info]');
+        this.searchField = $('#dataview-search-input');
+        this.searchButton = $('#dataview-search-button');
+        
+        this.gridName = $('#dataview-table-items-row td:nth-child(1)');
+        this.gridPhone = $('#dataview-table-items-row td:nth-child(2)');
+        this.gridEmail= $('#dataview-table-items-row td:nth-child(3)');
+        this.gridTotal = $('#dataview-table-items-row td:nth-child(4)');
+        this.gridEdit = $('#edit-button');
+        this.gridDelete = $('#delete-button');
+
         this.formName = element(by.name('name'));
         this.formPhone = element(by.name('phone'));
         this.formEmail = element(by.name('email'));
--- pages/invoices_po.js
+++ pages/invoices_po.js
@@ -4,10 +4,21 @@ class Invoices {
     constructor(){
         this.newInvoice = $('#dataview-insert-button');
         this.alertInfo = $('#content div[class*=alert-info]');
+        this.searchField = $('dataview-search-input');
+        this.searchButton = $('dataview-search-button');
+
+        this.gridCode = $('#dataview-table-items-row td:nth-child(1)');
+        this.gridDate = $('#dataview-table-items-row td:nth-child(2)');
+        this.gridCustomer = $('#dataview-table-items-row td:nth-child(3)');
+        this.gridTotal = $('#dataview-table-items-row td:nth-child(4)');
+        this.gridEdit = $('#edit-button');
+        this.gridDelete = $('#delete-button');
+
         this.formSave = $('#form-submit-button');
         this.formNumber = element(by.name('invoiceNumber'));
         this.formDate = element(by.name('date'));
         this.formCustomer = $('#customerId');
+        this.formCustomerSelect = $('.form-control option:nth-child(1)');
         this.formCancel = $('#form-cancel-button');
     }
     
